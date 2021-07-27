036deab43792d23ceb8ada0584e6ce8e0e23aa18
chrystian.amaral <chrystianamaral.zetta@ufla.br>
2021-07-27 18:42:17 -0300

    Primeiro commit

---
+++ backend/gestaodealunos/src/main/java/br/com/gestaodealunos/GestaoDeAlunosApplication.java
@@ -0,0 +1,13 @@
+package br.com.gestaodealunos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoDeAlunosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoDeAlunosApplication.class, args);
+	}
+
+}
