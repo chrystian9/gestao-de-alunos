77f838ba4e62c5e8865cf3ae30663d21222f7b77
Chrystian Amaral <chrystian.amaral@bitka.com.br>
2021-10-27 19:42:26 -0300

    Deploy da aplicação

--- gestaodealunos/src/main/java/br/com/gestaodealunos/GestaoDeAlunosApplication.java
+++ gestaodealunos/src/main/java/br/com/gestaodealunos/GestaoDeAlunosApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GestaodealunosApplication {
+public class GestaoDeAlunosApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GestaodealunosApplication.class, args);
+		SpringApplication.run(GestaoDeAlunosApplication.class, args);
 	}
 
 }
